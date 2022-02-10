5da78b6b9dd54988696aacb30a25c6030ada61f9
Dario0122 <dario.di.cuia@gmail.com>
2022-02-10 15:30:54 +0100

    install router & add blog page

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="container">
+        <h1>About</h1>
+        <p>lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+      <h1>Homepage</h1>
+      <p>lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
+
+<style>
+
+</style>
