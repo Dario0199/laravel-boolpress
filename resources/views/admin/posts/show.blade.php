36517e173fa35c63ffdc136ec90c590c70018ba1
Dario0122 <dario.di.cuia@gmail.com>
2022-02-11 17:07:42 +0100

    carbon

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,6 +4,10 @@
     <div class="container">
         <h1 class="mb-5">{{ $post->title }}</h1>
 
+        <h3 class="mb-3">{{ $post->created_at->format('l d/m/Y') }}</h3>
+        <h3 class="mb-3">{{ $post->created_at->isoformat('dddd DD/MM/YYYY') }}</h3>
+        <h3 class="mb-3">{{ $post->created_at->diffForHumans() }}</h3>
+
         <div class="mb-3">
             <p>Category: @if($post->category){{$post->category->name}} @else Uncategorized @endif</p>
             <a class="btn btn-success" href="{{ route('admin.posts.edit', $post->id) }}">Edit this post</a>
